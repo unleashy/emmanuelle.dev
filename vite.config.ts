8ba3087985f224d435bf24e073dc8531edb810a7
bnuuy.bsky.social <me@emmanuelle.dev>
2026-07-30 02:01:05 -0300

    Move svelte.config.js to vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "@sveltejs/adapter-static";
 
 export default defineConfig({
-  plugins: [sveltekit()],
-  server: {
-    fs: {
-      allow: [".yarn"],
-    },
-  },
+  plugins: [
+    sveltekit({
+      preprocess: vitePreprocess(),
+      adapter: adapter({
+        precompress: true,
+      }),
+    }),
+  ],
   css: {
     transformer: "lightningcss",
   },
